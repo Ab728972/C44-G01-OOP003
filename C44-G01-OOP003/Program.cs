97dd70c012ae1bdc02f7215fbd140e73be32f112
Abdullah Hasan <AbdullahAbdelhady929@gmail.com>
2025-08-15 07:07:56 +0300

    q4

--- C44-G01-OOP003/Program.cs
+++ C44-G01-OOP003/Program.cs
@@ -48,5 +48,20 @@
                 => new ComplexNumber(a.Real - b.Real, a.Imaginary - b.Imaginary);
         }
         #endregion
+        #region Q4 - Employee and Manager Classes
+        public class Employee
+        {
+            public virtual void Work() => Console.WriteLine("Employee is working");
+        }
+
+        public class Manager : Employee
+        {
+            public override void Work()
+            {
+                base.Work();
+                Console.WriteLine("Manager is managing");
+            }
+        }
+        #endregion
     }
 }
