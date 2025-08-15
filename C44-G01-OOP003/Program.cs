c6acf65201a0c0dcaec871630fb91152ee21f36d
Abdullah Hasan <AbdullahAbdelhady929@gmail.com>
2025-08-15 07:07:29 +0300

    q3

--- C44-G01-OOP003/Program.cs
+++ C44-G01-OOP003/Program.cs
@@ -29,5 +29,24 @@
             }
         }
         #endregion
+        #region Q3 - Complex Number Class
+        public class ComplexNumber
+        {
+            public double Real { get; set; }
+            public double Imaginary { get; set; }
+
+            public ComplexNumber(double real, double imaginary)
+            {
+                Real = real;
+                Imaginary = imaginary;
+            }
+
+            public static ComplexNumber operator +(ComplexNumber a, ComplexNumber b)
+                => new ComplexNumber(a.Real + b.Real, a.Imaginary + b.Imaginary);
+
+            public static ComplexNumber operator -(ComplexNumber a, ComplexNumber b)
+                => new ComplexNumber(a.Real - b.Real, a.Imaginary - b.Imaginary);
+        }
+        #endregion
     }
 }
