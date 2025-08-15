e0c8af0d839314d0d7101a3f3cedbf250de9571c
Abdullah Hasan <AbdullahAbdelhady929@gmail.com>
2025-08-15 07:06:32 +0300

    q1

--- C44-G01-OOP003/Program.cs
+++ C44-G01-OOP003/Program.cs
@@ -6,6 +6,13 @@
         {
             
         }
-
+        #region Q1 - Calculator Class
+        public class Calculator
+        {
+            public int Add(int a, int b) => a + b;
+            public int Add(int a, int b, int c) => a + b + c;
+            public double Add(double a, double b) => a + b;
+        }
+        #endregion
     }
 }
